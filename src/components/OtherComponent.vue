28e235b382caa4302a986ac113290b146ffcbba8
Camille Drapier <c.drapier@allm.inc>
2021-12-28 18:19:50 +0900

    Test keep-alive and q-img

---
+++ src/components/OtherComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" />
+</template>
+
+<script>
+export default {
+  name: 'OtherComponent'
+}
+</script>
