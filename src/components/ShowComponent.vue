40b3e292fc1807819ed9f2dc05668e0c1bd898b2
Camille Drapier <c.drapier@allm.inc>
2021-12-30 08:10:18 +0900

    Test q-img visible

--- src/components/ShowComponent.vue
+++ src/components/ShowComponent.vue
@@ -1,17 +1,9 @@
 <template>
-  <q-img v-show="visible" src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" />
+  <q-img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" />
 </template>
 
 <script>
 export default {
-  name: 'ShowComponent',
-  data () {
-    return {
-      visible: true
-    }
-  },
-  mounted () {
-    this.visible = false
-  }
+  name: 'ShowComponent'
 }
 </script>
